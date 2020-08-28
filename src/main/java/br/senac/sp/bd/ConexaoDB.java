fd6ef16de6f7c887a3c0b7583b934dd51cb3a747
yurin <yurin@DESKTOP-LU2U70E>
2020-08-27 21:53:14 -0300

    configuracão inicial

---
+++ src/main/java/br/senac/sp/bd/ConexaoDB.java
@@ -0,0 +1,30 @@
+package br.senac.sp.bd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ConexaoDB {
+
+    // Bloco Executado uma unica vez quando o servidor é inicializado
+    static {
+        try {
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ConexaoDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static Connection obterConexao() throws ClassNotFoundException, SQLException {
+        // url do banco de dados, muda de tipo de banco de dados
+        String url = "jdbc:derby://localhost:1527/senac";
+        //o usuario de acesso do banco ( estou usando mysql, geralmente é root)
+        String user = "senac";
+        //senha do banco
+        String senha = "senac";
+
+        return DriverManager.getConnection(url, user, senha);
+    }
+}
---
+++ src/main/java/br/senac/sp/dao/ClienteDAO.java
@@ -0,0 +1,41 @@
+package br.senac.sp.dao;
+
+import br.senac.sp.bd.ConexaoDB;
+import br.senac.sp.entidade.Cliente;
+import br.senac.sp.servlet.ServletDB;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ClienteDAO {
+
+    public static List<Cliente> getClientes() {
+        List<Cliente> listaClientes = new ArrayList();
+        try {
+            // faz a conexao com o Banco de dados criado na pasta bd
+            Connection con = ConexaoDB.obterConexao();
+            //query de requisicao do banco de dados
+            String query = "select * from cliente";
+            PreparedStatement ps = con.prepareStatement(query);
+            ResultSet resultado = ps.executeQuery();
+
+            // para mostrar na tela
+            while (resultado.next()) {
+                String nome = resultado.getString("nome");
+                String email = resultado.getString("email");
+                listaClientes.add(new Cliente(nome,email));
+
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ServletDB.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ServletDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return listaClientes;  
+    }
+}
